cccdab586b3846552b82bb004638433eab4b774f
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2026-01-20 08:16:00 +0100

    Feature: Implemented the solution.

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,60 @@
 public class Solution {
+    private Node head;
 
     public Solution() {
 
     }
 
     public void push(int val) {
+        /**
+         * If this is the very first value in the stack?
+         */
+        if (head == null) {
+            head = new Node(val, val, null);
+        } else {
+            /**
+             * min is always the lesser of current val and the min present in the next
+             * pointer node in the stack
+             * 
+             * here , the next pointer node in the stack is head
+             * 
+             * NOTE: Whenever a new pointer is added to the stack, the next pointer node in
+             * the stack is always updated.
+             */
+            head = new Node(val, Math.min(val, head.min), head);
+        }
 
     }
 
     public void pop() {
-
+        /**
+         * pointer node is updated to the next one in the stack
+         */
+        head = head.next;
     }
 
     public int top() {
-
+        return head.val;
     }
 
     public int getMin() {
+        return head.min;
 
     }
 
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
 }
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
